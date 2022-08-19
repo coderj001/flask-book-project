7ecc7d1c910ff06b340ce659fa118c7416d81892
raju ghorai <amirajubolchi001@gmail.com>
2022-08-19 23:44:51 +0530

    api added

--- api/v1/database/models.py
+++ api/v1/database/models.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, ListField, DecimalField
+from mongoengine import Document, ReferenceField, StringField, ListField, DecimalField
 
 
 class Todo(Document):
@@ -9,3 +9,12 @@ class Book(Document):
     book_name = StringField(required=True, max_length=200)
     category = ListField(StringField(max_length=100))
     rent_per_day = DecimalField(min_value=0)
+
+class Transaction(Document):
+    book = ReferenceField(Book)
+    date = StringField()
+    type = StringField(max_length=8,min_length=6)
+    person_name = StringField(max_length=100)
+    rent = DecimalField(min_value=0)
+
+    meta = {'allow_inheritance': True}
